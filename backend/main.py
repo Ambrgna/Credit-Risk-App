4cfe1ea9eaff5935b32d4f2eca8667d0f8933660
Ambrgna <mittens.pritchett@gmail.com>
2025-10-28 18:21:35 -0500

    setup api calls

---
+++ backend/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Allow frontend access (adjust port if needed)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI backend is running"}
+
+# Define the loan model
+class Loan(BaseModel):
+    person_age: int
+    person_income: float
+    person_homeownership: str
+    person_emplength: int
+    loan_intent: str
+    loan_grade: str
+    loan_amnt: float
+    loan_intrate: float
+    loan_percentincome: float
+    defaultonfile: bool
+    credhistlength: int
+
+# Mock ML model prediction
+@app.post("/predict/model1")
+def predict_model1(loan: Loan):
+    name = ""
+    result = ""
+    accuracy = 0
+
+    return {
+        "model_name": name,
+        "model_result": result,
+        "model_accuracy": accuracy
+        }
+
+@app.post("/predict/model2")
+def predict_model2(loan: Loan):
+    name = ""
+    result = 0
+    accuracy = 0
+
+    return {
+        "model_name": name,
+        "model_result": result,
+        "model_accuracy": accuracy
+        }
