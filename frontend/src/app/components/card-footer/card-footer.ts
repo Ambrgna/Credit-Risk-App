e6ce2807c98735ed39d68e574bcafb8f95a928d4
Ambrgna <mittens.pritchett@gmail.com>
2025-11-05 19:28:54 -0600

    worked on footer

--- frontend/src/app/components/card-footer/card-footer.ts
+++ frontend/src/app/components/card-footer/card-footer.ts
@@ -13,34 +13,8 @@ export class CardFooter {
 model1Image: string | null = null;
 model2Image: string | null = null;
 
-
-getAlertClass(value: number): string {
-  if (value >= 0 && value <= 33) return 'alert-danger';   // Red
-  if (value > 33 && value <= 66) return 'alert-warning'; // Yellow
-  return 'alert-success';                                  // Green
+getAlertClass(value: string): string {
+  if (value == "Rejected") return 'alert-danger';   // Red
+  return 'alert-success';                           // Green
 }
-
-getAverageResult(): number | null {
-  console.log(this.modelResults)
-  const results = this.modelResults;
-
-  if (results!==undefined&&results.length !== 0){
-    const sum = results.reduce((a, b) => a.model_accuracy + b.model_accuracy, 0);
-    return sum / results.length;
-  }
-   return null;
-}
-
-getAverageLabel(): string {
-  const avg = this.getAverageResult();
-  // return avg !== null ? this.getPredictionLabel(avg) : '';
-  return "";
-}
-
-getAverageAlertClass(): string {
-  const avg = this.getAverageResult();
-  return avg !== null ? this.getAlertClass(avg) : 'alert-secondary';
-}
-
-
 }
